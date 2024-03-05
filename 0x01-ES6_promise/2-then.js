62ee3abb50354de1cd5dd25b5b1197495860ea3d
Linf0rd <linfordsnagger@gmx.com>
2024-03-06 00:47:53 +0200

    Implement handleResponseFromAPI function
    Handles a Promise for an API response, logging success and returning a formatted response object.

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,8 @@
-export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
-      reject(new Error('The fake API is not working currently'));
-    }
-  });
+export default function handleResponseFromAPI(promise) {
+  promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => new Error());
 }
